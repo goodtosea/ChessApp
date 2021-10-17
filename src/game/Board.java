f4961d74c05f3390bbae6f28a815ccab445271c3
Nathan Nguyen <nathanvinhnguyen@gmail.com>
2021-10-16 18:07:01 -0700

    made method declarations and Javadoc comments

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,6 +1,34 @@
 package game;
 
+import pieces.Piece;
+
+/**
+ * A 2D array of pieces.
+ */
 public class Board
 {
-	
+    /**
+     * Constructs an empty 8x8 board.
+     */
+	public Board() {
+
+    }
+
+    /**
+     * Sets the value of a cell in the board to a specified piece.
+     * @param piece - the piece to set the cell's value to
+     * @param x - the column of the cell
+     * @param y - the row of the cell
+     */
+    void setPosition(Piece piece, int x, int y) {
+
+    }
+
+    /**
+     * Fills the board with pieces in the standard chess layout.
+     */
+    void fillboard() {
+
+    }
+
 }
