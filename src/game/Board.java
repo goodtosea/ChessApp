52f927e3634f5486e64060fafde8e03b8b94f56e
Mohib1402 <mohibkhan949@gmail.com>
2021-10-17 20:46:31 -0700

    Added a constructor for the Board constructor, will update it once we figure out a way to use the different pieces in the Piece class

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,6 +1,87 @@
 package game;
+import pieces.*;
 
 public class Board
 {
-	
+    private Piece boardArray[][];
+
+    public Board()
+    {
+        boardArray = new Piece[8][8];
+
+        /**
+         * White Pawns
+         */
+//        for(int j = 0; j < 8; j++)
+//            boardArray[1][j] =
+
+        /**
+         * Black Pawns
+         */
+//        for(int j = 0; j < 8; j++)
+//            boardArray[6][j] =
+
+        /**
+         * White Rooks
+         */
+//        for(int j = 0; j < 8; j = j + 7)
+//            boardArray[0][j] =
+
+
+        /**
+         * Black Rooks
+         */
+//        for(int j = 0; j < 8; j = j + 7)
+//            boardArray[7][j] =
+
+
+        /**
+         * White Knights
+         */
+//        for(int j = 1; j < 8; j = j + 5)
+//            boardArray[0][j] =
+
+
+        /**
+         * Black Knights
+         */
+//        for(int j = 1; j < 8; j = j + 5)
+//            boardArray[7][j] =
+
+        /**
+         * White Bishops
+         */
+//        for(int j = 2; j < 6; j = j + 3)
+//            boardArray[0][j] =
+
+        /**
+         * Black Bishops
+         */
+//        for(int j = 2; j < 6; j = j + 3)
+//            boardArray[7][j] =
+
+        /**
+         * White King
+         */
+//        boardArray[0][3] =
+
+        /**
+         * Black King
+         */
+//        boardArray[7][3] =
+
+        /**
+         * White Queen
+         */
+//        boardArray[0][4] =
+
+        /**
+         * Black Queen
+         */
+//        boardArray[7][4] =
+
+    }
+
+
+
 }
