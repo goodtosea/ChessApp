86477d0d4cc150113219401a198d81f537bf5d07
Nathan Nguyen <nathanvinhnguyen@gmail.com>
2021-10-31 11:57:04 -0700

    implemented Mover class

--- src/game/Mover.java
+++ src/game/Mover.java
@@ -7,11 +7,20 @@ import pieces.*;
  */
 public class Mover
 {
+    boolean isWhiteTurn;
+    King whiteKing;
+    King blackKing;
+
     /**
      * Constructs a mover.
+     * @param isWhiteTurn - the current turn
+     * @param whiteKing - the white king
+     * @param blackKing - the black king
      */
-    public Mover() {
-
+    public Mover(boolean isWhiteTurn, King whiteKing, King blackKing) {
+        this.isWhiteTurn = isWhiteTurn;
+        this.whiteKing = whiteKing;
+        this.blackKing = blackKing;
     }
 
     /**
@@ -21,8 +30,14 @@ public class Mover
      * @param y - the row to attempt to move to
      * @return true if the piece moves successfully, and false otherwise
      */
-    boolean tryMovePiece(Piece piece, int x, int y) {
+    protected boolean tryMovePiece(Piece piece, int x, int y) {
         // if (isValidMove(...)) then move
+        if (isValidMove(piece, x, y)) {
+            Board.setPosition(piece, x, y);
+            isWhiteTurn = !isWhiteTurn;
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -39,6 +54,38 @@ public class Mover
            (2) if it is the appropriate player's turn,
            (3) if the player's king is in check.
          */
+
+        boolean pieceIsWhite = piece.isWhite();
+        King playerKing = pieceIsWhite ? whiteKing : blackKing;
+
+        if (pieceIsWhite == isWhiteTurn && piece.isValidMove(x,y)) {
+
+            // if current player's king is not in check
+            if (!playerKing.isInCheck()) {
+                return true;
+            }
+            else {
+                // store initial position and piece at destination
+                int sourceX = piece.getX();
+                int sourceY = piece.getY();
+                Piece destinationPiece = Board.getBoardArray()[x][y];
+
+                // carry out the move
+                Board.setPosition(piece, x, y);
+
+                // check if player's king is still in check
+                boolean stillInCheck = playerKing.isInCheck();
+
+                // undo move
+                Board.setPosition(piece, sourceX, sourceY);
+                Board.setPosition(destinationPiece, x, y);
+
+                return !stillInCheck;
+            }
+
+        }
+
+
     }
 
 }
