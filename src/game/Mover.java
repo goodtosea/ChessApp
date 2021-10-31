7d05f09b2e2f699a1f4302190fd5e1128e3fdf8e
Nathan Nguyen <nathanvinhnguyen@gmail.com>
2021-10-31 12:19:00 -0700

    removed condition for if king is currently checked in isValidMove

--- src/game/Mover.java
+++ src/game/Mover.java
@@ -45,46 +45,31 @@ public class Mover
      * @param piece - the piece to check a move for
      * @param x - the column to simulate a move to
      * @param y - the row to simulate a move to
-     * @return
+     * @return true if move follows the rules of chess, and false otherwise
      */
     public boolean isValidMove(Piece piece, int x, int y) {
-        /*
-           check:
-           (1) if the specific piece can move that way,
-           (2) if it is the appropriate player's turn,
-           (3) if the player's king is in check.
-         */
 
         boolean pieceIsWhite = piece.isWhite();
         King playerKing = pieceIsWhite ? whiteKing : blackKing;
 
         if (pieceIsWhite == isWhiteTurn && piece.isValidMove(x,y)) {
+            // store initial position and piece at destination
+            int sourceX = piece.getX();
+            int sourceY = piece.getY();
+            Piece destinationPiece = Board.getBoardArray()[x][y];
 
-            // if current player's king is not in check
-            if (!playerKing.isInCheck()) {
-                return true;
-            }
-            else {
-                // store initial position and piece at destination
-                int sourceX = piece.getX();
-                int sourceY = piece.getY();
-                Piece destinationPiece = Board.getBoardArray()[x][y];
+            // carry out the move
+            Board.setPosition(piece, x, y);
 
-                // carry out the move
-                Board.setPosition(piece, x, y);
+            // check if player's king is in check
+            boolean isInCheck = playerKing.isInCheck();
 
-                // check if player's king is still in check
-                boolean stillInCheck = playerKing.isInCheck();
+            // undo move
+            Board.setPosition(piece, sourceX, sourceY);
+            Board.setPosition(destinationPiece, x, y);
 
-                // undo move
-                Board.setPosition(piece, sourceX, sourceY);
-                Board.setPosition(destinationPiece, x, y);
-
-                return !stillInCheck;
-            }
-
+            return !isInCheck;
         }
-
 
     }
 
