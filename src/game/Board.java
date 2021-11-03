6ed8ba2f2c1e9ab208ae97af76dcdabe3077be98
Alexander Lane <lnalex2456@gmail.com>
2021-11-03 09:45:33 -0700

    fixed the ctors to have the isWhite parameter

--- src/game/Board.java
+++ src/game/Board.java
@@ -39,49 +39,49 @@ public class Board
 
         // white pawns
         for (int j = firstPieceCol; j < firstPieceCol + 8; j++) {
-            setPosition(new Pawn(j, 1), j, 1);
+            setPosition(new Pawn(j, 1, true), j, 1);
         }
 
         // black pawns
         for (int j = firstPieceCol; j < firstPieceCol + 8; j++) {
-            setPosition(new Pawn(j, topRow - 1), j, topRow - 1);
+            setPosition(new Pawn(j, topRow - 1, false), j, topRow - 1);
         }
 
         // white rooks
-        setPosition(new Rook(firstPieceCol, 0), firstPieceCol, 0);
-        setPosition(new Rook(firstPieceCol + 7, 0), firstPieceCol + 7, 0);
+        setPosition(new Rook(firstPieceCol, 0, true), firstPieceCol, 0);
+        setPosition(new Rook(firstPieceCol + 7, 0, true), firstPieceCol + 7, 0);
 
         // black rooks
-        setPosition(new Rook(firstPieceCol, topRow), firstPieceCol, topRow);
-        setPosition(new Rook(firstPieceCol + 7, topRow), firstPieceCol + 7, topRow);
+        setPosition(new Rook(firstPieceCol, topRow, false), firstPieceCol, topRow);
+        setPosition(new Rook(firstPieceCol + 7, topRow, false), firstPieceCol + 7, topRow);
 
         // white knights
-        setPosition(new Knight(firstPieceCol + 1, 0), firstPieceCol + 1, 0);
-        setPosition(new Knight(firstPieceCol + 6, 0), firstPieceCol + 6, 0);
+        setPosition(new Knight(firstPieceCol + 1, 0, true), firstPieceCol + 1, 0);
+        setPosition(new Knight(firstPieceCol + 6, 0, true), firstPieceCol + 6, 0);
 
         // black knights
-        setPosition(new Knight(firstPieceCol + 1, topRow), firstPieceCol + 1, topRow);
-        setPosition(new Knight(firstPieceCol + 6, topRow), firstPieceCol + 6, topRow);
+        setPosition(new Knight(firstPieceCol + 1, topRow, false), firstPieceCol + 1, topRow);
+        setPosition(new Knight(firstPieceCol + 6, topRow, false), firstPieceCol + 6, topRow);
 
         // white bishops
-        setPosition(new Bishop(firstPieceCol + 2, 0), firstPieceCol + 2, 0);
-        setPosition(new Bishop(firstPieceCol + 5, 0), firstPieceCol + 5, 0);
+        setPosition(new Bishop(firstPieceCol + 2, 0, true), firstPieceCol + 2, 0);
+        setPosition(new Bishop(firstPieceCol + 5, 0, true), firstPieceCol + 5, 0);
 
         // black bishops
-        setPosition(new Bishop(firstPieceCol + 2, topRow), firstPieceCol + 2, topRow);
-        setPosition(new Bishop(firstPieceCol + 5, topRow), firstPieceCol + 5, topRow);
+        setPosition(new Bishop(firstPieceCol + 2, topRow, false), firstPieceCol + 2, topRow);
+        setPosition(new Bishop(firstPieceCol + 5, topRow, false), firstPieceCol + 5, topRow);
 
         // white queen
-        setPosition(new Queen(firstPieceCol + 3, 0), firstPieceCol + 3, 0);
+        setPosition(new Queen(firstPieceCol + 3, 0, true), firstPieceCol + 3, 0);
 
         // black queen
-        setPosition(new Queen(firstPieceCol + 3, topRow), firstPieceCol + 3, topRow);
+        setPosition(new Queen(firstPieceCol + 3, topRow, false), firstPieceCol + 3, topRow);
 
         // white king
-        setPosition(new Queen(firstPieceCol + 4, 0), firstPieceCol + 4, 0);
+        setPosition(new Queen(firstPieceCol + 4, 0, true), firstPieceCol + 4, 0);
 
         // black king
-        setPosition(new Queen(firstPieceCol + 4, topRow), firstPieceCol + 4, topRow);
+        setPosition(new Queen(firstPieceCol + 4, topRow, false), firstPieceCol + 4, topRow);
 
     }
     
