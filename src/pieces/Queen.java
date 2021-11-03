c4c719455923fee17ea7678c4911fecd2d68c392
goodtosea <lnalex2456@gmail.com>
2021-11-03 09:34:04 -0700

    used rook and bishop logic for queen with collisions move

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -1,4 +1,5 @@
 package pieces;
+import game.Board;
 
 /**
  * Chess piece that moves and captures diagonally, vertically, and horizontally.
@@ -24,8 +25,74 @@ public class Queen extends Piece
     @Override
     public boolean isValidMove(int x, int y) {
     	// If it is a valid rook or bishop move, it's a valid queen move
-    	if ((Math.abs(x - this.x) == Math.abs(y - this.y)) || ((x == this.x) || (y == this.y)))
-    		return true;
+    	// Copy pasted bishop move
+    	if ((Math.abs(x - this.x) == Math.abs(y - this.y))) {
+    		int pathLength = Math.abs(x - this.x);
+        	if (pathLength != Math.abs(y - this.y) || pathLength == 0) 
+        		return false;
+        	
+        	// Determines the direction for the for loop that checks each space
+        	int stepDirectionX = Integer.signum(x - this.x);
+        	int stepDirectionY = Integer.signum(y - this.y);
+        	
+        	Piece[][] b = Board.getBoardArray();
+        	
+        	// Check that each cell between the start and end point is empty
+        	for (int i = 1; i < pathLength; i++) {
+        		int curr_x = this.x + stepDirectionX * i;
+        		int curr_y = this.y + stepDirectionY * i;
+        		
+        		if (b[curr_x][curr_y] == null)
+        			continue;
+        		else
+        			return false;
+        	}
+        	
+        	// Check that if the end point is full, that it is the opposite color
+        	// Board needs to be filled for the access to work
+        	if (b[x][y] != null)
+        		if (isWhite == b[x][y].isWhite())
+        			return false;
+        	return true;
+    	}
+    	// Copy pasted rook logic
+    	else if (x == this.x ^ y == this.y)
+    	{
+    		// Pick the change that's greatest (one will always be 0 in a legal move)
+    		int deltaX = Math.abs(x - this.x);
+        	int deltaY = Math.abs(y - this.y);
+        	int pathLength = deltaX == 0 ? deltaY : deltaX;
+        	
+        	// No movement isn't a valid move
+        	if (pathLength == 0)
+        		return false;
+        	
+        	// Works when difference is 0
+        	// Determines the direction for the for loop that checks each space
+        	int stepDirectionX = Integer.signum(deltaX);
+        	int stepDirectionY = Integer.signum(deltaY);
+        	
+        	Piece[][] b = Board.getBoardArray();
+        	
+        	// When one is 0, it doesn't get changed here
+        	// Check that each cell between the start and end point is empty
+        	for (int i = 1; i < pathLength; i++) {
+        		int curr_x = this.x + stepDirectionX * i;
+        		int curr_y = this.y + stepDirectionY * i;
+        		
+        		if (b[curr_x][curr_y] == null)
+        			continue;
+        		else
+        			return false;
+        	}
+        	
+        	// Check that if the end point is full, that it is the opposite color
+        	// Board needs to be filled for the access to work
+        	if (b[x][y] != null)
+        		if (isWhite == b[x][y].isWhite())
+        			return false;
+        	return true;
+    	}
     	return false;
     }
     
