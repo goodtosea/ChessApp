0537efc735b54fce3b57561c5565010a2418f856
goodtosea <lnalex2456@gmail.com>
2021-11-03 16:47:33 -0700

    added to queen test case

--- test/tests/QueenTests.java
+++ test/tests/QueenTests.java
@@ -1,5 +1,6 @@
 package tests;
 import pieces.Queen;
+import game.Board;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,10 +14,10 @@ class QueenTests
 	void queenValidMoveTest()
 	{
 		Queen q = new Queen(4, 8, true);
-		assertEquals(q.isValidMove(4, 7), true);	// Up-right 1
-		assertEquals(q.isValidMove(4, 8), true);	// Up 1
-		assertEquals(q.isValidMove(5, 6), true);	// Up-right 2
-		assertEquals(q.isValidMove(2, 7), true);	// Up-left 1
+		Board b = new Board();
+		assertEquals(q.isValidMove(4, 7), true);	// Down 1
+		assertEquals(q.isValidMove(4, 8), false);	// Same position
+		assertEquals(q.isValidMove(2, 7), false);	// Left 2 Down 1
 		assertEquals(q.isValidMove(0, 0), false);
 	}
 
