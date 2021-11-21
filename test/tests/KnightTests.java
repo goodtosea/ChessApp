eb0a541f704937f4c800944d1ea3bf66d3e5424c
goodtosea <lnalex2456@gmail.com>
2021-11-20 18:34:35 -0800

    Added tests for knight, mover and king

--- test/tests/KnightTests.java
+++ test/tests/KnightTests.java
@@ -14,7 +14,13 @@ class KnightTests
 	{
 		new Board();
 		Knight k = new Knight(3, 4, true);
-		
+		Pawn p = new Pawn(2, 6, true);
+		Board b = new Board();
+		Board.setPosition(k, 3, 4);
+		Board.setPosition(p, 2, 6);
+		assertEquals(false, k.isValidMove(2, 6));
+		assertEquals(true, k.isValidMove(4, 6));
+		assertEquals(false, k.isValidMove(2, 5));
 	}
 
 }
